2bd9457af51597774847b1d16e6507603f32aa9f
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-10-14 23:13:09 +0100

    implement local storage on the todo list app

--- script.js
+++ script.js
@@ -4,15 +4,37 @@ document.addEventListener("DOMContentLoaded",()=>{
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
+    
+
+    // Load tasks from Local Storage
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+        function loadTasks() {
+            tasks.forEach(item => {
+                const task = document.createElement("li");
+                task.textContent = item;
+                taskList.appendChild(task);
+                console.log(tasks.value);
+
+                button =document.createElement("button");
+                button.textContent = "Remove";
+                button.classList.add("remove-btn");
+                button.addEventListener("click", ()=>{
+                taskList.removeChild(task);
+            });
+                task.appendChild(button);
+                taskList.appendChild(task);
+            });
+        }
+    
 
     // This function help us handle the adding task process and the removal of each one. Since every task is added with his removal button
     function addTask() {
-        const taskText = taskInput.value.trim();
-    
+       const taskText = taskInput.value.trim();
+
         if(taskText == ""){
             alert("Enter a task name");
         }
-
         else{
             const task = document.createElement("li");
             task.textContent = taskText;
@@ -27,14 +49,29 @@ document.addEventListener("DOMContentLoaded",()=>{
             });
             task.appendChild(button);
             taskList.appendChild(task);
+            //save the task to the local storage
+            saveTasks();
             taskInput.value = "";
         }
-        
+
     }
-        addButton.addEventListener("click", addTask);
-        taskInput.addEventListener("keypress",(event)=>{
-            if (event.key == "Enter") addTask() ;
-        });
+
+        function saveTasks() {
+            localStorage.setItem('tasks', JSON.stringify(tasks)); // Save tasks to Local Storage
+            console.log(tasks);
+        }
+
+     
+            taskInput.addEventListener("keypress",(event)=>{
+                if (event.key == "Enter") addTask() ;
+    
+            });
+            addButton.addEventListener("click", ()=>{addTask()});
+       
+        
+            loadTasks();
+        // Other initialization code
+    
        
    
 });
